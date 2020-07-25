6d70087fab13d3ec8de29111ae190e9519f5431f
赵晨 <zhaochen18@meituan.com>
2020-07-25 16:02:59 +0800

    1

---
+++ src/main/java/com/sankuai/flink/Test.java
@@ -0,0 +1,15 @@
+package com.sankuai.flink;
+
+/**
+ * @description:
+ * @author:zhaochen18@meituan.com
+ * @create:2020-07-25 15:56
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+
+}
